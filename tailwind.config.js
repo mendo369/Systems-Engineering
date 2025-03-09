db6bbf078fc419932d34d2de185565cafcbfaee2
Luis Mendoza <luis23luis33@gmail.com>
2025-03-09 00:15:33 -0500

    custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        main:{
+          green:"#a3e636b3"
+        }
+      }
+    },
   },
   plugins: [],
 }
