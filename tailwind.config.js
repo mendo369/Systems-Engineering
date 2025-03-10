3d7b947e767debd1e163ba87a6d37b309b301121
Luis Mendoza <luis23luis33@gmail.com>
2025-03-09 22:55:41 -0500

    animation frames

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        main:{
-          green:"#a3e636b3"
-        }
-      }
+      colors: {
+        main: {
+          green: "#a3e636b3",
+        },
+      },
+      animation: {
+        marquee1: "marquee1 20s linear infinite",
+        marquee2: "marquee2 20s linear infinite",
+      },
+      keyframes: {
+        marquee1: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
