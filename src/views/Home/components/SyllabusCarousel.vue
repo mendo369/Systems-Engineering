5c4fd3c520097b756516ab5909106ae03e024207
Luis Mendoza <luis23luis33@gmail.com>
2025-03-09 23:20:14 -0500

    change hero image

--- src/views/Home/components/SyllabusCarousel.vue
+++ src/views/Home/components/SyllabusCarousel.vue
@@ -3,7 +3,8 @@
 
 <template>
   <div class="">
-    <img src="https://files.idyllic.app/files/static/2316882" alt="" class="w-[600px]">
+    <img src="https://cdni.iconscout.com/illustration/premium/thumb/employee-works-on-business-puzzle-illustration-download-in-svg-png-gif-file-formats--call-logo-solutions-challenges-miscellaneous-pack-illustrations-9674530.png" alt="" class="w-[800px]">
+    <!-- <img src="https://cdni.iconscout.com/illustration/premium/thumb/application-programming-and-software-user-interface-creation-process-illustration-download-in-svg-png-gif-file-formats--web-development-pack-design-illustrations-8957004.png" alt="" class="w-[600px]"> -->
     <!-- <img src="https://cdni.iconscout.com/illustration/premium/thumb/student-prepares-for-exam-illustration-download-in-svg-png-gif-file-formats--test-preparation-graduation-miscellaneous-pack-illustrations-9674537.png?f=webp" alt=""> -->
     <!-- <img src="https://cdni.iconscout.com/illustration/premium/thumb/programmer-works-on-website-application-illustration-download-in-svg-png-gif-file-formats--development-web-developer-miscellaneous-pack-illustrations-9674528.png?f=webp" alt=""> -->
   </div>
