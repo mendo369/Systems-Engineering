09e1e7e9fb12b10d41366002260d46b012e0c10c
Luis Mendoza <luis23luis33@gmail.com>
2025-03-09 23:02:44 -0500

    change Syllabus component by Render component

---
+++ src/components/Render.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+<section class="w-full flex justify-center overflow-y-auto overflow-x-hidden  h-full scrollbar-none">
+    <RouterView></RouterView>
+</section>
+</template>
+
+
+<style scoped>
+.scrollbar-none{
+    scrollbar-width: none;
+}
+</style>
