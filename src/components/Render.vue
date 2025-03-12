e291917d837dce98a7e26f62014983a506eaa411
Luis Mendoza <luis23luis33@gmail.com>
2025-03-11 23:54:16 -0500

    transition animation

--- src/components/Render.vue
+++ src/components/Render.vue
@@ -3,14 +3,30 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-<section class="w-full flex justify-center overflow-y-auto overflow-x-hidden  h-full scrollbar-none">
-    <RouterView></RouterView>
-</section>
+  <section
+    class="w-full flex justify-center overflow-y-auto overflow-x-hidden h-full scrollbar-none"
+  >
+    <router-view v-slot="{ Component }">
+      <transition name="slide" mode="out-in">
+        <component :is="Component" :key="$route.path" />
+      </transition>
+    </router-view>
+  </section>
 </template>
 
-
 <style scoped>
-.scrollbar-none{
-    scrollbar-width: none;
+.scrollbar-none {
+  scrollbar-width: none;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 1s, transform 1s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
 }
 </style>
