a63e2bdd2d82e9575b799e79bba87792d3d45914
Luis Mendoza <luis23luis33@gmail.com>
2025-03-11 23:56:03 -0500

    OS and LinearAlgebra routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,12 +6,34 @@ const routes: RouteRecordRaw[] = [
     path: "/",
     name: "home",
     component: () => import("../views/Home/Home.vue"),
+  },
+  {
+    path: "/operating-systems",
+    name: "operating-systems",
+    meta: { transition: 'slide-left' },
+    component: () => import("../views/OS/OperatingSystems.vue"),
+  },
+  {
+    path: "/events-programming",
+    name: "events-programming",
+    component: () => import("../views/EventsProgramming/EventsProgramming.vue"),
+  },
+  {
+    path: "/objects-programming",
+    name: "objects-programming",
+    component: () => import("../views/OOP/ObjectProgramming.vue"),
+  },
+  {
+    path: "/linear-algebra",
+    name: "linear-algebra",
+    component: () => import("../views/LinearAlgebra/LinearAlgebra.vue"),
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  linkActiveClass: 'bg-main-green'
 });
 
 export default router;
