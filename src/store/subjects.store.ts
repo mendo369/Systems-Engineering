44365b47a89e6b936ed303425c2e177d51e07cb2
Luis Mendoza <luis23luis33@gmail.com>
2025-03-11 23:58:23 -0500

    change variables name

--- src/store/subjects.store.ts
+++ src/store/subjects.store.ts
@@ -1,26 +1,22 @@
 import { defineStore } from "pinia"
-import type {  Subject } from "../models/subject.model"
-import CareersData from '../data/careers.json' // Import estático
+import SubjectsData from '../data/subjects.json' // Import estático
 
 export const useSubjectsStore = defineStore("subjects", {
     state: () => ({
         subjects: [] as Array<{name:string, url:string}>
     }),
     actions: {
-        // Opción 1: Usar el import estático (más simple)
         async loadSubjects() {
             await this.fetchSubjects()
             return
         },
-
-        // Opción 2: Usar fetch (si necesitas cargar datos dinámicamente)
         async fetchSubjects() {
             try {
-               const careers = CareersData.map(career=>{
-                return {name:career.name, url:career.url}
+               const subjects = SubjectsData.map(subject=>{
+                return {name:subject.name, url:subject.url}
                })
 
-               this.subjects = careers
+               this.subjects = subjects
 
                return 
             } catch (error) {
