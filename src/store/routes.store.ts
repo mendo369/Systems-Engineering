3569ef6d4ae2943004182bc54dbcaf34ce332517
Luis Mendoza <luis23luis33@gmail.com>
2025-03-11 23:57:11 -0500

    routes store

---
+++ src/store/routes.store.ts
@@ -0,0 +1,32 @@
+import { defineStore } from "pinia"
+import SubjectsData from '../data/subjects.json' // Import estático
+
+export const useRoutesStore = defineStore("routes", {
+    state: () => ({
+        subjects: [] as Array<{name:string, url:string}>
+    }),
+    actions: {
+        async loadSubjects() {
+            await this.fetchSubjects()
+            return
+        },
+        async fetchSubjects() {
+            try {
+               const subjects = SubjectsData.map(subject=>{
+                return {name:subject.name, url:subject.url}
+               })
+
+               this.subjects = subjects
+
+               return 
+            } catch (error) {
+                console.error('Error al cargar las asignaturas:', error)
+                this.subjects = []
+                return []
+            }
+        }
+    },
+    getters: {
+        getAllSubjects: (state) => state.subjects
+    }
+})
