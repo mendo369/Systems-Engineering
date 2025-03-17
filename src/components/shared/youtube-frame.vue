fb8a57bec69982b1c0865236af80c3e794f68997
Luis Mendoza <luis23luis33@gmail.com>
2025-03-17 12:11:48 -0500

    Youtube frame component

---
+++ src/components/shared/youtube-frame.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps({ videoUrl: String });
+</script>
+
+<template>
+  <div class="w-[560px] mx-auto my-4">
+    <iframe
+      class="w-full h-full"
+      :src="videoUrl"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<style scoped>
+div {
+  aspect-ratio: 16/9;
+}
+</style>
